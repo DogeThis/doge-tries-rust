c0b2a9e2b7cddd948adfc00c0fc5db4874fa5473
The Doge <alex.ren@gmail.com>
2023-07-03 17:02:01 -0700

    first writing attempt

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,28 @@ fn main() {
         println!("Value for bundle_path: {}", bundle_path.display());
         let bundle = Bundle::load(bundle_path);
         match bundle {
-            Ok(bundle) => inspect_bundle(&bundle),
+            Ok(bundle) => inspect_bundle(bundle),
             Err(err) => println!("Error: {}", err),
         }
     }
 }
 
-fn inspect_bundle (bundle: &Bundle) {
+fn inspect_bundle (mut bundle: Bundle) {
     let cab = bundle.get_cab();
     let whatever = bundle.files();
-    for (_what, bundle_file) in whatever {
+
+    for (what, bundle_file) in whatever {
+        println!("what is this: {:#?}", what);
         match bundle_file {
         BundleFile::Assets(asset_file) => {
                 
+            let my_materials = asset_file.assets.iter().filter_map(|asset| {
+                match asset {
+                    Asset::Material(material) => Some(material),
+                    _ => None,
+                }
+            });
+
             asset_file.assets.iter().for_each(|asset| {
                     match asset {
                         Asset::Material(material) => {
@@ -48,6 +57,32 @@ fn inspect_bundle (bundle: &Bundle) {
             _ => {} //println!("whatever"),
         }
     }
-    println!("CAB: {:#?}", cab);
+    let mutbundle: Option<&mut BundleFile> = bundle.get_mut("CAB-d14402932a96240520bc659742cab91e");
+    if let Some(bundle_file) = mutbundle {
+        println!("hi let's mutate this thing");
+        match bundle_file {
+            BundleFile::Assets(asset_file) => {
+                println!("hello");
+                asset_file.assets.iter_mut().for_each(|asset| {
+                    match asset {
+                        Asset::Material(material) => {
+                            let shader = &mut material.shader;
+                            println!("Shader: {:#?}", shader);
+                            shader.file_id = 1337;
+                            shader.path_id = 42;
+                        },
+                        Asset::Bundle(bundle) => {
+                            println!("Bundle name: {:#?}", bundle.name);
+                            println!("Bundle deps: {:#?}", bundle.dependencies);
+                        },
+                        _ => {} // println!("Other don't care"),
+                    }
+                });
+            },
+            _ => {}
+        }
+    }
+    bundle.save("hithere").expect("whateve");
+    // println!("CAB: {:#?}", cab);
     // println!("Bundle: {:#?}", bundle);
 }
